0cc4a699278f257b69342fd0381bf55ed91fe3a9
Ashwin Balakrishna <abalakrishna@Ashwins-MacBook-Pro.local>
2017-02-15 01:36:26 -0800

    shitty initial version workin

--- attempt1.py
+++ attempt1.py
@@ -3,6 +3,7 @@ import networkx as nx
 import numpy as np
 import sim
 import operator
+from operator import itemgetter
 
 ############################################
 # Load graph and convert to networkx graph #
@@ -32,12 +33,19 @@ def get_top_cluster(k):
     sorted_clust = np.array(sorted(clust_dict.items(), key=operator.itemgetter(1)), dtype=str)
     return np.ndarray.tolist(sorted_clust[-k:,0])
 
+def get_top_degree(k):
+    degree_dict = G.degree(G.nodes())
+    sorted_degree_dict = np.array( sorted(degree_dict.items(), key = itemgetter(1)  ), dtype = str)
+    return np.ndarray.tolist(sorted_degree_dict[-k:, 0])
+
 def run_strategy(k, strat_name):
     if strat_name == 'random':
         return get_random(k)
     elif strat_name == 'cluster':
         return get_top_cluster(k)
-    else
+    elif strat_name == 'degree':
+        return get_top_degree(k)
+    else:
         return []
 
 #######################
@@ -49,21 +57,16 @@ def repeat_strategy(k, num_iters, strat_name):
         choices.append(run_strategy(k, strat_name))
     return choices
 
-
 def run_simulation(k, strat_name):
     nodes = {}
     for name in list(strat_name.keys()):
-        nodes[name] = run_strategy(k, strat_name)
+        nodes[name] = run_strategy(k, strat_name[name])
     print(sim.run(data, nodes))
 
-# run_simulation(5, {'strat1': 'random', 'strat2': 'random'})
-
+run_simulation(5, {'strat1': 'degree', 'strat2': 'random'})
 
 def print_out(choices, outfile_path):
     f = open(outfile_path, 'w')
     for iter_choices in choices:
         for node in iter_choices:
             f.write(str(node) + '\n')
-
-#choices = repeat_strategy(5, 2, 'random')
-#print_out(choices, './outputtest.txt')
