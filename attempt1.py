12c523a6fac0c39df5218cb604068b593c9c6816
Anshul Ramachandran <anshulr28@gmail.com>
2017-02-17 22:15:43 -0800

    stuff

--- attempt1.py
+++ attempt1.py
@@ -4,12 +4,14 @@ import numpy as np
 import sim
 from operator import itemgetter
 import time
+import random
 import matplotlib.pyplot as plt
 
 ############################################
 # Load graph and convert to networkx graph #
 ############################################
-with open('graphs/4.10.1.json') as data_file:
+#with open('graphs/testgraph1.json') as data_file:
+with open('/Users/anshulramachandran/Downloads/8.35.2.json') as data_file:
 # with open('./testgraph1.json') as data_file:
     data = json.load(data_file)
 
@@ -22,46 +24,46 @@ for i in range(num_nodes):
         adj[i][int(neighbor)] = 1
 
 G = nx.from_numpy_matrix(adj)
-# G=nx.dodecahedral_graph()
-pos = nx.spring_layout(G)
-nx.draw(G)
-plt.show()
+# # G=nx.dodecahedral_graph()
+# pos = nx.spring_layout(G)
+# #nx.draw(G)
+# #plt.show()
+#
+# G1 = G.copy()
+# old_len = -1
+# while len(G1.nodes()) != old_len:
+#     old_len = len(G1.nodes())
+#     deg = G1.degree()
+#     to_remove = [n for n in deg if deg[n] <= 10]
+#     G1.remove_nodes_from(to_remove)
+#     print old_len
+# #nx.draw_networkx_nodes(G1, pos)
+# #nx.draw_networkx_edges(G1, pos)
+# #plt.show()
+#
+# bw_centrality_dict = nx.betweenness_centrality(G1)
+# # dg_centrality_dict = nx.degree_centrality(G1)
+# alpha =1
+# ranking = {}
+# for key in bw_centrality_dict.keys():
+#     ranking[key] = alpha * bw_centrality_dict[key]
+# print ranking
+# ranking = np.array(sorted(ranking.items(), key=itemgetter(1)))
+# drop = np.ndarray.tolist(ranking[:, 0])
+# while nx.is_connected(G1):
+#     print(len(G1.nodes()))
+#     if (drop[-1] in G1.nodes()):
+#         G1.remove_node(drop[-1])
+#     drop = drop[:-1]
+#     deg = G1.degree()
+#     to_remove = [n for n in deg if deg[n] <= 1]
+#     G1.remove_nodes_from(to_remove)
+# # G1.remove_nodes_from(drop)
+# print drop
 
-G1 = G.copy()
-old_len = -1
-while len(G1.nodes()) != old_len:
-    old_len = len(G1.nodes())
-    deg = G1.degree()
-    to_remove = [n for n in deg if deg[n] <= 10]
-    G1.remove_nodes_from(to_remove)
-    print old_len
-nx.draw_networkx_nodes(G1, pos)
-nx.draw_networkx_edges(G1, pos)
-plt.show()
-
-bw_centrality_dict = nx.betweenness_centrality(G1)
-# dg_centrality_dict = nx.degree_centrality(G1)
-alpha =1
-ranking = {}
-for key in bw_centrality_dict.keys():
-    ranking[key] = alpha * bw_centrality_dict[key]
-print ranking
-ranking = np.array(sorted(ranking.items(), key=itemgetter(1)))
-drop = np.ndarray.tolist(ranking[:, 0])
-while nx.is_connected(G1):
-    print(len(G1.nodes()))
-    if (drop[-1] in G1.nodes()):
-        G1.remove_node(drop[-1])
-    drop = drop[:-1]
-    deg = G1.degree()
-    to_remove = [n for n in deg if deg[n] <= 1]
-    G1.remove_nodes_from(to_remove)
-# G1.remove_nodes_from(drop)
-print drop
-
-nx.draw_networkx_nodes(G1, pos)
-nx.draw_networkx_edges(G1, pos)
-plt.show()
+#nx.draw_networkx_nodes(G1, pos)
+#nx.draw_networkx_edges(G1, pos)
+#plt.show()
 ####################
 # Strategy Section #
 ####################
@@ -93,6 +95,29 @@ def get_centrality_sum(k):
         centrality_dict[key] = dg_centrality_dict[key] + cl_centrality_dict[key] + bw_centrality_dict[key]
     sorted_centrality_dict = np.array( sorted(centrality_dict.items(), key = itemgetter(1)  ), dtype = str)
     return np.ndarray.tolist(sorted_centrality_dict[-k:, 0])
+
+def get_centrality_sum_randomized_multiple(k, distr, num_iters):
+    dg_centrality_dict = nx.degree_centrality(G)
+    cl_centrality_dict = nx.closeness_centrality(G)
+    bw_centrality_dict = nx.betweenness_centrality(G)
+    centrality_dict = {}
+    for key in bw_centrality_dict.keys():
+        centrality_dict[key] = dg_centrality_dict[key] + cl_centrality_dict[key] + bw_centrality_dict[key]
+    sorted_centrality_dict = np.array( sorted(centrality_dict.items(), key = itemgetter(1)  ), dtype = str)
+    topk = [i[0] for i in sorted_centrality_dict[-k:]]
+    middle2k = [i[0] for i in sorted_centrality_dict[-3*k: -k]]
+    bottom3k = [i[0] for i in sorted_centrality_dict[-6*k: -3*k]]
+    numtopk = int(float(k) * distr[0] / sum(distr))
+    nummiddle2k = int(float(k) * distr[1] / sum(distr))
+    numbottom3k = k - numtopk - nummiddle2k
+    choices = []
+    for i in range(num_iters):
+        random.shuffle(topk)
+        random.shuffle(middle2k)
+        random.shuffle(bottom3k)
+        choices.append(topk[0:numtopk] + middle2k[0:nummiddle2k] + bottom3k[0:numbottom3k])
+    return choices
+
 
 def run_strategy(k, strat_name):
     if strat_name == 'random':
@@ -170,5 +195,9 @@ def print_out(choices, outfile_path):
         for node in iter_choices:
             f.write(str(node) + '\n')
 
+choices = repeat_strategy(35, 50, 'random')
+print_out(choices, '/Users/anshulramachandran/Downloads/submission8.35.2.1.txt')
 # choices = repeat_same_strategy(35, 50, 'centrality')
-# print_out(choices, '/Users/anshulramachandran/Downloads/submission8.35.1.2.txt')
+choices = get_centrality_sum_randomized_multiple(35, [7,14,14], 50)
+#print_out(choices, './output1.txt')
+print_out(choices, '/Users/anshulramachandran/Downloads/submission8.35.2.2.txt')
